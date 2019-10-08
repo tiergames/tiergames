735a1a74b112a5abdf0cbbe94fa0d02b55e322d1
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-10-08 20:35:11 +0200

    Fix public imports on the server

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
 const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
@@ -60,7 +59,6 @@ app.use(require('node-sass-middleware')({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 hbs.registerHelper('ifUndefined', (value, options) => {
