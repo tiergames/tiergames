194017bd5fe82f081e48ab4977858528d7211042
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-10-08 20:51:19 +0200

    Add the call to the api when the component mount

--- client/src/pages/private/Games/Games.jsx
+++ client/src/pages/private/Games/Games.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Games extends Component {
   render() {
@@ -7,5 +8,13 @@ export default class Games extends Component {
         <h1>Games</h1>
       </div>
     )
+  }
+
+  componentDidMount() {
+    axios.post(
+      "http://localhost:3001/api/games?offset=0&limit=10"
+    ). then(games => {
+      console.log(games)
+    })
   }
 }
