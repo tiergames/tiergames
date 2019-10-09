f18eb1735411d4bae0b0f837ee4bba8bb6e8e432
Maximiliano Sussini <maxsus60@gmail.com>
2019-10-09 18:24:06 +0200

    Platform service done. Get all platforms

---
+++ client/src/pages/private/Platforms/Platforms.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import PlatformsService from './../../../services/platforms.service'
+
+export default class Platform extends Component {
+  constructor() {
+    super()
+    this.service = new PlatformsService()
+    
+    this.state = {
+      platforms: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Platforms</h1>
+        <ul className="platforms-list">
+        {
+          (this.state.platforms.length > 0)
+          ? (
+            this.state.platforms.map(platform => {
+              return <li key={platform.id}>{platform.name}</li>
+            })
+          )
+          : null
+        }
+        </ul>
+      </div>
+    )
+  }
+
+  async componentDidMount() {
+    let allPlatforms = await this.service.platforms()
+    this.setState({
+      ...this.state,
+      platforms: allPlatforms,
+    })
+  }
+}
