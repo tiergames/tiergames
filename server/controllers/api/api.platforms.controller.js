f4f0a7b259c54c51994ec55a63ad2721eca0b30a
Maximiliano Sussini <maxsus60@gmail.com>
2019-10-09 17:58:04 +0200

    Platform service done. Get all platforms

---
+++ server/controllers/api/api.platforms.controller.js
@@ -0,0 +1,27 @@
+const controller = {};
+
+const platformsURL = "https://api-v3.igdb.com/platforms";
+const axios = require("axios");
+
+controller.platforms = (req, res, next) => {
+  axios({
+    url: platformsURL,
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "user-key": process.env.IGDB_API_KEY
+    },
+    data:
+      "fields name,platform_logo; limit 50;"
+  })
+    .then(response => {
+      res.status(200).json(response.data);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err.message
+      });
+    });
+};
+
+module.exports = controller;
