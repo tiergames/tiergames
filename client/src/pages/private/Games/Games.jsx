88b52f47fb88622bf6e41e57950e1e068e45335b
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-10-10 12:00:02 +0200

    Add the genres filter to the Games page. Also separate the games request and the genres request into separate async functions called from the componentDidMount function

--- client/src/pages/private/Games/Games.jsx
+++ client/src/pages/private/Games/Games.jsx
@@ -2,58 +2,57 @@ import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 import GameService from './../../../services/games.service'
+import GenresService from './../../../services/genres.service'
 
 export default class Games extends Component {
   constructor(props) {
     super()
-    this.service = new GameService()
+    this.gamesService = new GameService()
+    this.genresService = new GenresService()
     this.state = {
       pagination: {
         limit: 10,
         offset: 0,
         currentPage: 0,
       },
-      isLoadingGames: true,
       games: [],
-      gamesFiltered: []
+      gamesFiltered: [],
+      isLoadingGames: true,
+      isLoadingGenres: true,
+      genres: []
     }
   }
   
   render() {
     return (
-      <div>
-        <h1>Games</h1>
-        <ul className="games-list">
-          {this.state.games.length > 0
-            ? 
-              this.state.games.map(game => {
-                return <li key={game.id}>{game.name}</li>
-              })
-            : null
-          }
-        </ul>
-
-        {this.state.isLoadingGames
-          ?
-            <Link to={"#"}>
-              Loading...
-            </Link>
-          :
-            <Link to={"#"} onClick={() => this.loadNextGames()}>
-              Load more
-            </Link>
-        }
-      </div>
+      <>
+        {this.renderGenres()}
+        {this.renderGames()}
+      </>
     )
   }
 
-  async componentDidMount() {
-    let allGames = await this.service.getGames(this.state.pagination.limit, this.state.pagination.offset)
+  async loadGames() {
+    let allGames = await this.gamesService.getGames(this.state.pagination.limit, this.state.pagination.offset)
     this.setState({
       ...this.state,
       games: allGames.data,
       isLoadingGames: false
     })
+  }
+
+  async loadGenres() {
+    let allGenres = await this.genresService.getAllGenres()
+    this.setState({
+      ...this.state,
+      genres: allGenres.data,
+      isLoadingGenres: false
+    })
+  }
+
+  componentDidMount() {
+    this.loadGames()
+    this.loadGenres()
   }
 
   async loadNextGames() {
@@ -65,7 +64,7 @@ export default class Games extends Component {
     let nextPage = this.state.pagination.currentPage + 1
     let nextOffset = this.state.pagination.limit * nextPage
     
-    let nextNewGames = await this.service.getGames(this.state.pagination.limit, nextOffset)
+    let nextNewGames = await this.gamesService.getGames(this.state.pagination.limit, nextOffset)
     let newGames = [...this.state.games]
     let newPagination = {...this.state.pagination}
     newPagination.offset = nextOffset
@@ -79,7 +78,56 @@ export default class Games extends Component {
     })
   }
 
-  renderGames() {
+  renderGenres() {
+    return (
+      <section>
+        <h2>Genres</h2>
+        {this.state.isLoadingGenres
+          ?
+            <p>Loading genres...</p>
+          :
+            <form className="filter games-filter">
+              {this.state.genres.map(genre => {
+                return (
+                  <div className="field field-checkbox">
+                    <input type="checkbox" name="genre" id={genre.id}/>
+                    <label htmlFor={genre.id}>{genre.name}</label>
+                  </div>
+                )
+              })}
+              <div className="form-actions">
+                <button type="submit">Apply filter</button>
+              </div>
+            </form>
+        }
+      </section>
+    )
+  }
 
+  renderGames() {
+    return (
+      <section>
+        <h2>Games</h2>
+        <ul className="games-list">
+          {this.state.games.length > 0
+            ? 
+              this.state.games.map(game => {
+                return <li key={game.id}>{game.name}</li>
+              })
+            : null
+          }
+        </ul>
+        {this.state.isLoadingGames
+          ?
+            <Link to={"#"}>
+              Loading...
+            </Link>
+          :
+            <Link to={"#"} onClick={() => this.loadNextGames()}>
+              Load more
+            </Link>
+        }
+      </section>
+    )
   }
 }
