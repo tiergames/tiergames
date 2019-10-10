b961cfcda2a915bea7d9bf41f8a873e9778f24be
Maximiliano Sussini <maxsus60@gmail.com>
2019-10-10 20:49:23 +0200

    Fixed 'coming' files

--- server/routes/api.coming.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/api/api.coming.controller");
-
-router.get("/", (req, res, next) => {
-  controller.coming(req, res, next);
-});
-
-module.exports = router;
