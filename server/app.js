374187e2945f3e8f4bc347146be6417d6ea6501d
Maximiliano Sussini <maxsus60@gmail.com>
2019-10-14 19:16:56 +0200

    heroku try again

--- server/app.js
+++ server/app.js
@@ -38,6 +38,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 // Enable authentication using session + passport
 app.use(session({
   secret: 'irongenerator',
